5a55330287cf117608e720f31c81de3b3ad0daca
jesse <jesse@universitypccare.com>
2020-01-05 19:31:43 -0500

    update test.php with more tests
    Signed-off-by: jesse <jesse@universitypccare.com>

--- tests/test.php
+++ tests/test.php
@@ -7,4 +7,15 @@ require_once("test_declarations.php");
 $gsx = new UPCC\GSXHandler($userEmail, $shipTo);
 $gsx->setActivationToken($activationToken);
 $gsx->testAuthentication();
-var_dump($gsx->ProductDetails($serial));
+
+$product = $gsx->ProductDetails($serial);
+$repair = $gsx->RepairSummary($repairNumber);
+$article = $gsx->ArticleContentLookup($articleId);
+$diagnostics = $gsx->DiagnosticsSuites($serial);
+
+echo "<pre>";
+echo "Product is a $product->configDescription\n";
+echo "Retrieved $repair->totalNumberofRecords repairs\n";
+echo "Article title is $article->title\n";
+echo "Diagnostics Suite available: " . $diagnostics->suiteDetails[0]->suiteName . "\n";
+echo "</pre>";
